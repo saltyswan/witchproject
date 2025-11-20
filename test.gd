46a9a4e6f85ef0e4d0efb5680652c6130cafde19
saltyswan <wannab.cassio@gmail.com>
2025-11-21 00:04:56 +0100

    J17
    Knockback FIXED
    Mouse vs. joystick FIXED
    ! Aim with joystick is weird
    Skeletons added

--- test.gd
+++ test.gd
@@ -1,7 +1,4 @@
 extends Node2D
-
-var hearts_list : Array[TextureRect]
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -11,7 +8,3 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_button_pressed() -> void:
-	$Hud/HBoxContainer/Heart1.visible = false
